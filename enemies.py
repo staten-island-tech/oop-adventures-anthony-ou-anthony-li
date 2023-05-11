fb0ac49fe427806953f235aa51c632ebbd4c4214
anthonyli525 <anthonyl510@nycstudents.net>
2023-05-11 08:39:17 -0400

    Update enemies.py

--- enemies.py
+++ enemies.py
@@ -1,33 +1,29 @@
-#Enemies
+##Characters
 class character:
-    def __init__(self, health, name):
+    def __init__(self, health, name, damage):
         self.health = health
         self.name = name
-class MainCharacter(character):
+        self.damage = damage
+class Player(character):
     def __init__(self):
-        super().__init__(health=50, name="CAPT> SPACE BOY")
+        super().__init__(health=150, name="CAPT> SPACE BOY", damage = 0)
 class Mechs(character):
     def __init__(self):
-        super().__init__(health=50, name="Zombodroid")
-        self.damage = 50
+        super().__init__(health=40, name="Zombodroid", damage = 20)
 class Bloater(character):
     def __init__(self):
-        super().__init__(health=20, name="Bloater")
-        self.damage = 10
+        super().__init__(health=20, name="Bloater", damage = 10)
 class Regurgitator(character):
     def __init__(self):
-        super().__init__(health=10, name="Regurgitator")
-        self.damage = 10
+        super().__init__(health=20, name="Regurgitator", damage = 10)
 class Necrosis(character):
     def __init__(self):
-        super().__init__(health=10, name="Necrosis")
-        self.damage = 15
+        super().__init__(health=20, name="Necrosis", damage = 15)
 class Mothership(character):
     def __init__(self):
-        super().__init__(health=100, name="Mothership")
-        self.damage = 20
+        super().__init__(health=100, name="Mothership", damage = 20)
 ##Attack
-def mech_attack(self):
-        print(self.damage(MainCharacter)-self.damage(Mechs))
+def mech_attack(damage):
+    health = Player(damage)-Mechs(damage)
+    print(f"Your current health is {health}")
 
-mech_attack(Mechs)
