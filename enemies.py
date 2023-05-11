d6f1206f0bc1a9aba730cd0d1d47dea6173ddae1
anthonyli525 <anthonyl510@nycstudents.net>
2023-05-11 08:16:39 -0400

    Update enemies.py

--- enemies.py
+++ enemies.py
@@ -26,3 +26,8 @@ class Mothership(character):
     def __init__(self):
         super().__init__(health=100, name="Mothership")
         self.damage = 20
+##Attack
+def mech_attack(self):
+        print(self.damage(MainCharacter)-self.damage(Mechs))
+
+mech_attack()
