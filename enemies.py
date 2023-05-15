24579a853fa6f64840a59079421ad25a35f4987b
anthonyli525 <anthonyl510@nycstudents.net>
2023-05-15 08:31:38 -0400

    Update enemies.py

--- enemies.py
+++ enemies.py
@@ -6,23 +6,30 @@ class character:
         self.damage = damage
 class Player(character):
     def __init__(self, health, name, damage):
-        super().__init__(health=150, name="CAPT> SPACE BOY", damage = 0)
+        super().__init__(health ,name, damage)
+        self.health = health
+        self.name = name
+        self.damage = damage
 class Bloater(character):
     def __init__(self, health, name, damage):
-        super().__init__(health=20, name="Bloater", damage = 10)
+        super().__init__(health ,name, damage)
+        self.health = health
+        self.name = name
+        self.damage = damage
 class Regurgitator(character):
-    def __init__(self, health, name, damage):
+    def __init__(self):
         super().__init__(health=20, name="Regurgitator", damage = 10)
 class Necrosis(character):
-    def __init__(self, health, name, damage):
+    def __init__(self):
         super().__init__(health=20, name="Necrosis", damage = 15)
 class Mothership(character):
-    def __init__(self, health, name, damage):
+    def __init__(self):
         super().__init__(health=100, name="Mothership", damage = 20)
-enemy = Bloater
-def enemy_attack():
-    if enemy == Bloater:
-        health = Player.health - Bloater.damage
-        print(health)
 
-    enemy_attack()
+def bloater_attack():
+    Bloater(20, "Bloater", 10)
+    Player(150, "CAPT> SPACE BOY, 0")
+    health = Player.health - Bloater.damage
+    print(f"Your current health after the attack is {health}.")
+
+    bloater_attack()
