ac289d56239725db433dac5ca5453475e1b0751d
anthonyli525 <anthonyl510@nycstudents.net>
2023-05-15 08:05:00 -0400

    Update enemies.py

--- enemies.py
+++ enemies.py
@@ -5,22 +5,24 @@ class character:
         self.name = name
         self.damage = damage
 class Player(character):
-    def __init__(self):
+    def __init__(self, health, name, damage):
         super().__init__(health=150, name="CAPT> SPACE BOY", damage = 0)
 class Bloater(character):
-    def __init__(self):
+    def __init__(self, health, name, damage):
         super().__init__(health=20, name="Bloater", damage = 10)
 class Regurgitator(character):
-    def __init__(self):
+    def __init__(self, health, name, damage):
         super().__init__(health=20, name="Regurgitator", damage = 10)
 class Necrosis(character):
-    def __init__(self):
+    def __init__(self, health, name, damage):
         super().__init__(health=20, name="Necrosis", damage = 15)
 class Mothership(character):
-    def __init__(self):
+    def __init__(self, health, name, damage):
         super().__init__(health=100, name="Mothership", damage = 20)
+enemy = Bloater
+def enemy_attack():
+    if enemy == Bloater:
+        health = Player.health - Bloater.damage
+        print(health)
 
-    def bloat_attack():
-        print(Player.health - Bloater.damage)
-
-    bloat_attack()
+    enemy_attack()
