3bc3522ad2d69a9f2079ca299b2aea9b8cdbac0f
anthonyli525 <anthonyl510@nycstudents.net>
2023-05-16 07:54:00 -0400

    Update enemies.py

--- enemies.py
+++ enemies.py
@@ -27,9 +27,9 @@ class Mothership(character):
         super().__init__(health=100, name="Mothership", damage = 20)
 
 def bloater_attack():
-    Bloater(20, "Bloater", 10)
-    Player(150, "CAPT> SPACE BOY, 0")
-    health = Player.health - Bloater.damage
+    enemy = Bloater(20, "Bloater", 10)
+    player =Player(150, "CAPT> SPACE BOY, 0")
+    health = player.health - enemy.damage
     print(f"Your current health after the attack is {health}.")
 
     bloater_attack()
