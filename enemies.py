7d7d9d0f2b149ecb4d47b2ecd3bb7cb7ea6173c1
anthonyli525 <anthonyl510@nycstudents.net>
2023-05-16 08:14:58 -0400

    Update enemies.py

--- enemies.py
+++ enemies.py
@@ -20,10 +20,24 @@ class Mothership(character):
     def __init__(self, health, name, damage):
         super().__init__(health, name, damage)
         
-    bloater = Bloater(20, "Bloater", 10)
-    player = Player(150, "CAPT> SPACE BOY", 0)
-    regurgitator = Regurgitator(20, "Regurgitator", 10)
-    necrosis = Necrosis(20, "Necrosis", 15)
-    Mothership = Mothership(100, "Mothership", 20)
+bloater = Bloater(20, "Bloater", 10)
+player = Player(150, "CAPT> SPACE BOY", 0)
+regurgitator = Regurgitator(20, "Regurgitator", 10)
+necrosis = Necrosis(20, "Necrosis", 15)
+mothership = Mothership(100, "Mothership", 20)
 
-    
+enemy = bloater
+def enemy_attack():
+    if enemy == bloater:
+        health = player.health - bloater.damage
+        print(f"Your current health now is {health}")
+    elif enemy == regurgitator:
+        health = player.health - regurgitator.damage
+        print(f"Your current health now is {health}")
+    elif enemy == necrosis:
+        health = player.health - necrosis.damage
+        print(f"Your current health now is {health}")
+    else:
+        print("There are no enemies in your sight.")
+
+enemy_attack()
