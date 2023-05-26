3c904e5899f375ec67b41948f0db19218beaea6c
anthonyo-git <anthonyou23@gmail.com>
2023-05-26 07:54:51 -0400

    Update app.py

--- app.py
+++ app.py
@@ -2,62 +2,90 @@ import random
 from time import sleep
 import sys
 
-#Character
+# Character
+
+
 class character:
-    def __init__(self, health, name, damage):
+    def __init__(self, health, name):
         self.health = health
         self.name = name
-        self.damage = damage
+
+
 class Captain(character):
     def __init__(self):
-        super().__init__(health=150, name="Captain", damage = 0)
-    def __str__(self):
-        return f"{self.health}, {self.name}, {self.damage}"
+        super().__init__(health=150, name="Captain")
 
- #Aliens   
+    def __str__(self):
+        return f"{self.health}, {self.name}"
+
+ # Aliens
+
+
 class Aliens:
     def __init__(self, health, name, damage):
         self.health = health
         self.name = name
         self.damage = damage
+
+
 class Bloater(Aliens):
     def __init__(self):
-        super().__init__(health=20, name="Bloater", damage = 10)
-    def __str__(self):
-        return f"{self.name}, {'health'}: {self.health}, {'damage'}: {self.damage}"
-class Regurgitator(Aliens):
-    def __init__(self):
-        super().__init__(health=10, name="Regurgitator", damage = 10)
-    def __str__(self):
-        return f"{self.name}, {'health'}: {self.health}, {'damage'}: {self.damage}"
-class Necrosis(Aliens):
-    def __init__(self):
-        super().__init__(health=10, name="Necrosis", damage = 15)
+        super().__init__(health=20, name="Bloater", damage=10)
+
     def __str__(self):
         return f"{self.name}, {'health'}: {self.health}, {'damage'}: {self.damage}"
 
-#Weapons
+
+class Regurgitator(Aliens):
+    def __init__(self):
+        super().__init__(health=10, name="Regurgitator", damage=10)
+
+    def __str__(self):
+        return f"{self.name}, {'health'}: {self.health}, {'damage'}: {self.damage}"
+
+
+class Necrosis(Aliens):
+    def __init__(self):
+        super().__init__(health=10, name="Necrosis", damage=15)
+
+    def __str__(self):
+        return f"{self.name}, {'health'}: {self.health}, {'damage'}: {self.damage}"
+
+# Weapons
+
+
 class weapon:
     def __init__(self, damage, name):
         self.damage = damage
         self.name = name
+
+
 class Rifle(weapon):
     def __init__(self):
         super().__init__(damage=10, name="Rifle")
+
     def __str__(self):
         return f"{self.name}: {self.damage} {'damage'}"
+
+
 class Sniper(weapon):
     def __init__(self):
         super().__init__(damage=15, name="Sniper")
+
     def __str__(self):
         return f"{self.name}: {self.damage} {'damage'}"
+
+
 class SMG(weapon):
     def __init__(self):
         super().__init__(damage=10, name="SMG")
+
     def __str__(self):
         return f"{self.name}: {self.damage} {'damage'}"
 
 # Start of game
+
+
 class space:
     def __init__(self):
         self.Main_Character = Captain()
@@ -81,13 +109,13 @@ class space:
             self.current_weapon = self.get_weapon(weapon_choice)
             if self.current_weapon == None:
                 print('Invalid weapon choice, please enter in a valid weapon')
-    
+
     def get_weapon(self, name):
         available_weapons = [Rifle(), Sniper(), SMG()]
         for weapon_choice in available_weapons:
             if weapon_choice.name.lower() == name.lower():
                 return weapon_choice
-            
+
     def display_weapon(self):
         if self.current_weapon is not None:
             print('Your chosen weapon: ')
@@ -103,16 +131,16 @@ class space:
         print('B. Run away')
         choice = input('Pick your choice (A/B): ').upper()
 
-    #Error Handling 
-        while choice not in ['A','B']:
+    # Error Handling
+        while choice not in ['A', 'B']:
             print('Invalid input, please enter A or B')
             choice = input('Pick your choice (A/B): ').upper()
-        
+
         # Handle choice A
         if choice == 'A':
             print('Be careful to dodge the asteroids, make sure to avoid collisions')
-            while choice == 'A': #loop until 2
-                num = random.randint(1,2)
+            while choice == 'A':  # loop until 2
+                num = random.randint(1, 2)
                 if num != 2:
                     print('Navigating through Asteroid Field')
                     sleep(3)
@@ -121,13 +149,14 @@ class space:
                     sleep(4)
                     print('Passing Asteroid field')
                     sleep(1.5)
-                    print('You have successfully passed the Asteroid Field, you have now reached the ship')
+                    print(
+                        'You have successfully passed the Asteroid Field, you have now reached the ship')
                     break
 
             # Handle choice B
         elif choice == 'B':
-            while choice == 'B': #loop until 2
-                num = random.randint(1,2)
+            while choice == 'B':  # loop until 2
+                num = random.randint(1, 2)
                 if num != 2:
                     sleep(3)
                     print('Running away')
@@ -137,25 +166,20 @@ class space:
                     break
         if choice == 'B':
             sys.exit('GAME OVER')
-        
-    def player_v_alien(self):
-        player = Captain().health
-        aliens_killed = 0
-    
-    #Loop for until you die or Aliens killed is 10 
-        while player > 0 and  aliens_killed < 10:
-            alien_choose = random.choice(self.aliens)
-    # Display Aleins info 
-            print(Aliens.name)
-            print(f"A {alien_choose} is coming after you")
-            break
 
-        
-                
+    def player_v_alien(self):
+        player = Captain
+        aliens_killed = 0
+
+        while player.health > 150 and aliens_killed < 10:
+            random.choice(self.aliens)
+            print(f"A {Aliens.name} has appeared")
+
 
 
 def main():
     game = space()
     game.player_v_alien()
+
 
 main()
